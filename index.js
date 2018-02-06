d0c4e5ff8ca8c569d0845aa56eeaa78a7e16dcad
Corentin Hatte <contact@corentin-hatte.eu>
2018-02-06 16:33:03 +0100

    Use optional templates for options & fix baseURL

--- index.js
+++ index.js
@@ -1,4 +1,137 @@
-const JD_TextEditor_BUTTONS = {
+class JD_TextEditor {
+	constructor(textarea, options) {
+		this.textarea = textarea;
+
+		if (typeof options === 'undefined') {
+			options = {};
+		}
+		
+		this.baseURL = options.baseUrl || '';
+
+		if (Array.isArray(options.buttons)) {
+			this.buttons = options.buttons;
+		}
+
+		if (typeof options.buttonsAreaClassName === 'string') {
+			this.buttonsAreaClassName = options.buttonsAreaClassName;
+		}
+		else {
+			this.buttonsAreaClassName = 'jd-text-editor-buttons';
+		}
+
+		if (options.buttonsTpl && document.getElementById(options.buttonsTpl)) {
+			this.buttonsTpl = document.getElementById(options.buttonsTpl);
+		}
+		if (typeof this.buttonsTpl === 'string') {
+			const str = this.buttonsTpl;
+			this.buttonsTpl = document.createElement('template');
+			this.buttonsTpl.innerHTML = str;
+		}
+
+		if (options.singleButtonTpl && document.getElementById(options.singleButtonTpl)) {
+			this.singleButtonTpl = document.getElementById(options.singleButtonTpl);
+		}
+		if (typeof this.singleButtonTpl === 'string') {
+			const str = this.singleButtonTpl;
+			this.singleButtonTpl = document.createElement('template');
+			this.singleButtonTpl.innerHTML = str;
+		}
+
+		this.createTextareaCopy();
+
+		this.createButtonsContainer();
+
+		this.createButtons();
+	}
+
+	createTextareaCopy() {
+		this.editorArea = document.createElement('div');
+		this.editorArea.className = this.textarea.className;
+		this.editorArea.contentEditable = true;
+		this.editorArea.style.minHeight = this.textarea.offsetHeight + 'px';
+
+		this.editorArea.innerHTML = this.textarea.value;
+		this.editorArea.innerHTML = this.setURLs();
+
+		this.textarea.parentNode.insertBefore(this.editorArea, this.textarea);
+
+		try {
+			document.execCommand('styleWithCSS', false, false);
+		}
+		catch (e) {
+			// do nothing
+		}
+
+		this.editorArea.addEventListener('input', () => {
+			this.textarea.value = this.reverseURLs();
+		}, false);
+
+		this.textarea.setAttribute('hidden', true);
+	}
+
+	createButtonsContainer() {
+		this.buttonsContainer = document.importNode(this.buttonsTpl.content, true);
+
+		this.containerButtonsParent = this.buttonsContainer.querySelector('[data-inject-buttons]');
+
+		this.editorArea.parentNode.insertBefore(this.buttonsContainer, this.editorArea);
+	}
+	createButtons() {
+		if (this.buttons) {
+
+			Object.entries(this.buttons).forEach((entry) => {
+				const [id, button] = entry;
+
+				const btnTpl = document.createElement('template');
+
+				let btnTplStr = this.singleButtonTpl.innerHTML.replace(/\{\{( )?id( )?\}\}/gi, id);
+
+				Object.keys(button).forEach((key) => {
+					btnTplStr = btnTplStr.replace(new RegExp('{{( )?' + key + '( )?}}', 'gi'), button[key]);
+				});
+
+				btnTpl.innerHTML = btnTplStr;
+
+				const btn = document.importNode(btnTpl.content, true);
+
+				const buttonListener = btn.querySelector('[data-button]');
+				if (buttonListener) {
+					buttonListener.addEventListener('click', (e) => {
+						e.preventDefault();
+
+						if (button.command) {
+							document.execCommand(button.command, false, null);
+						}
+						if (button.customCommand) {
+							button.customCommand.call(this, button);
+						}
+
+						if (this.baseURL) {
+							this.editorArea.innerHTML = this.setURLs();
+							this.textarea.value = this.reverseURLs();
+						}
+						else {
+							this.textarea.value = this.editorArea.innerHTML;
+						}
+					}, false);
+				}	
+
+				this.containerButtonsParent.appendChild(btn);
+			});
+		}
+
+	}
+
+	setURLs() {
+		return this.editorArea.innerHTML.replace(new RegExp('(src|href)="((?!http|https|ftp|//)[^"]+)"', 'gi'), '$1="' + this.baseURL + '$2"');
+	}
+
+	reverseURLs() {
+		return this.editorArea.innerHTML.replace(new RegExp('(src|href)="' + this.baseURL + '([^"]+)"', 'gi'), '$1="$2"');
+	}
+}
+
+JD_TextEditor.prototype.buttons = {
 	bold: {
 		content: '𝐁',
 		icon: 'format_bold',
@@ -53,126 +186,9 @@ const JD_TextEditor_BUTTONS = {
 	}
 };
 
-class JD_TextEditor {
-	constructor(textarea, options) {
-		if (typeof options === 'undefined') {
-			options = {};
-		}
-		
-		if (typeof options.baseURL === 'string') {
-			this.baseURL = options.baseURL;
-		}
+JD_TextEditor.prototype.buttonsTpl = '<div class="jd-buttons-container" data-inject-buttons></div>';
+JD_TextEditor.prototype.singleButtonTpl = '<button type="button" class="jd-button" title="{{ title }}" data-button data-icon="format_{{ id }}">{{ content }}</button> ';
 
-		if (Array.isArray(options.buttons)) {
-			this.buttons = options.buttons;
-		}
-		else {
-			this.buttons = JD_TextEditor_BUTTONS;
-		}
-
-		if (typeof options.buttonsAreaClassName === 'string') {
-			this.buttonsAreaClassName = options.buttonsAreaClassName;
-		}
-		else {
-			this.buttonsAreaClassName = 'jd-text-editor-options';
-		}
-
-		if (typeof options.buttonsClassName === 'string') {
-			this.buttonsClassName = options.buttonsClassName;
-		}
-		else if (Array.isArray(options.buttonsClassName)) {
-			this.buttonsClassName = options.buttonsClassName.join(' ');
-		}
-		else {
-			this.buttonsClassName = 'button';
-		}
-
-		this.textarea = textarea;
-
-		if (this.buttons) {
-			this.buttonsArea = document.createElement('div');
-			this.buttonsArea.className = this.buttonsAreaClassName;
-			this.textarea.parentNode.insertBefore(this.buttonsArea, this.textarea);
-
-			Object.values(this.buttons).forEach((button) => {
-				var btn = document.createElement('button');
-
-				if (this.buttonsClassName) {
-					btn.className = this.buttonsClassName;
-				}
-
-				if (button.className) {
-					btn.classList.add.apply(btn.classList, button.className.split(' '));
-				}
-				if (button.title) {
-					btn.title = button.title;
-				}
-				if (button.icon) {
-					btn.dataset.icon = button.icon;
-				}
-				if (button.content) {
-					btn.innerHTML = button.content;
-				}
-
-				btn.addEventListener('click', (e) => {
-					e.preventDefault();
-
-					if (button.command) {
-						document.execCommand(button.command, false, null);
-					}
-					if (button.customCommand) {
-						button.customCommand.call(this, button);
-					}
-
-					if (this.baseURL) {
-						this.editorArea.innerHTML = this.setURLs();
-						this.textarea.value = this.reverseURLs();
-					}
-					else {
-						this.textarea.value = this.editorArea.innerHTML;
-					}
-				}, false);
-
-				this.buttonsArea.appendChild(btn);
-				this.buttonsArea.appendChild(document.createTextNode(' '));
-			});
-		}
-
-		this.editorArea = document.createElement('pre');
-		this.editorArea.className = this.textarea.className;
-		this.editorArea.contentEditable = true;
-		this.editorArea.style.minHeight = this.textarea.offsetHeight + 'px';
-		this.editorArea.innerHTML = this.textarea.value;
-		this.textarea.parentNode.insertBefore(this.editorArea, this.textarea);
-
-		try {
-			document.execCommand('styleWithCSS', false, false);
-		}
-		catch (e) {
-			// do nothing
-		}
-
-
-		if (this.baseURL) {
-			this.setURLs = function () {
-				return this.editorArea.innerHTML.replace(new RegExp('(src|href)="([^http|https|ftp|//][^"]+)"', 'gi'), '$1="' + this.baseURL + '$2"');
-			};
-
-			this.reverseURLs = function () {
-				return this.editorArea.innerHTML.replace(new RegExp('(src|href)="' + this.baseURL + '([^"]+)"', 'gi'), '$1="$2"');
-			};
-
-			this.editorArea.addEventListener('input', () => {
-				this.textarea.value = this.reverseURLs();
-			}, false);
-
-			this.editorArea.innerHTML = this.setURLs();
-		}
-
-		this.textarea.setAttribute('hidden', true);
-	}
-}
-
-document.querySelectorAll('textarea.jd-text-editor').forEach((area) => {
-	new JD_TextEditor(area);
+document.querySelectorAll('textarea[data-jd-text-editor]').forEach((area) => {
+	new JD_TextEditor(area, area.dataset);
 });
